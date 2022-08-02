40621008428591d497c2aa8b756b97373444ecf4
Derek <70810073+Invvk@users.noreply.github.com>
2022-08-02 03:50:15 -0700

    add arithmetic expression test

--- src/test/java/io/github/invvk/dstest/stack/StackTest.java
+++ src/test/java/io/github/invvk/dstest/stack/StackTest.java
@@ -1,7 +1,11 @@
 package io.github.invvk.dstest.stack;
 
 import io.github.invvk.ds.stack.Stack;
+import io.github.invvk.ds.stack.arithmetic.ExpressionCalculator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +25,13 @@ public class StackTest {
         int expected = 1;
         stack.push(expected);
         assertEquals(expected, stack.pop());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'9*(5+4/2-1)*3',162", "'1+1',2", "'1+2*2+1',6"})
+    public void validateArithmetic(String expression, int expectedResult) {
+        int result = ExpressionCalculator.calculate(expression);
+        assertEquals(expectedResult, result);
     }
 
 }
